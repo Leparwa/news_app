b4d337cc0086ed46435ac71738555602acbd76a7
leparwa <kevin.leparwa@student.moringaschool.com>
2021-08-11 12:24:44 +0300

    project setup

---
+++ app.py
@@ -0,0 +1,11 @@
+from os import name
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('news_page.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
